6f7ff8cb60ebfc67ddb6bb088d6bcabc0cecc3e8
tobibako45 <tobibako45@gmail.com>
2019-10-04 19:59:09 +0900

    chapter2

--- chapter_2.rb
+++ chapter_2.rb
@@ -44,3 +44,33 @@ puts Gear.new(52, 11).ratio # -> 4.7272727272727275
 puts Gear.new(30, 27).ratio # -> 1.1111111111111112
 puts "########################################"
 
+
+
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @rim = rim
+    @tire = tire
+  end
+
+  def ratio # 比率
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    # タイヤはリムの周りを囲むので、直径を計算するためには２倍にする
+    ratio * (rim + (tire * 2))
+  end
+
+end
+
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+puts Gear.new(52, 11, 26, 1.25).gear_inches
+
+puts "########################################"
+
+
+puts Gear.new(52, 11).ratio
